cd24f89f430c40507ebbe8eedbf681a0e565c527
Brian Vargas <brian.vargas17@gmail.com>
2020-10-16 23:59:03 -0500

    arrays for passwords created

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,18 @@
 // Get user info from prompts
-let length = prompt ("How many characters do you need?");
-let lowerCase = confirm ("Would you like lower case letters?");
-let upperCase = confirm ("Would you like upper case letters?");
-let numbers = confirm ("Would you like numbers?");
-let specialCharacters = confirm("Would you like special characters?");
+let desiredLength = prompt ("How many characters do you need?");
+let desiredLowerCase = confirm ("Would you like lower case letters?");
+let desiredUpperCase = confirm ("Would you like upper case letters?");
+let desiredNumbers = confirm ("Would you like numbers?");
+let desiredSpecialCharacters = confirm("Would you like special characters?");
 
+// Create arrays
+let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let numbers = ["0","1","2","3","4","5","6","7","8","9","0"];
+let specialCharacters = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+
+
+console.log(specialCharacters)
 
 
 
