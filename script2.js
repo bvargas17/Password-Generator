e158ba84313a70ae0ff420df721ad2cbcb6948fb
Brian Vargas <brian.vargas17@gmail.com>
2020-10-17 22:55:14 -0500

    made script check for a number between 8 and 128 and verify it a number

--- script2.js
+++ script2.js
@@ -13,101 +13,22 @@ function generatePassword() {
   let desiredUpperCase = confirm("Would you like upper case letters?");
   let desiredNumbers = confirm("Would you like numbers?");
   let desiredSpecialCharacters = confirm("Would you like special characters?");
-  let desiredLength = prompt("How many characters do you need?");
+  let desiredLength = prompt("How many characters do you need between 8 and 128?");
+    if (desiredLength < 8 || desiredLength > 128 ) {
+      alert ("Lenght needs to be within 8 and 128 characters.");
+      return;
+    };
+    if ( Number.isNaN(desiredLength) === false ){
+      alert ("Needs to be a number.");
+    };
+
   let howLong = desiredLength * 1;
 
   // Create arrays
-  let lowerCase = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  let upperCase = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  let specialCharacters = [
-    "!",
-    '"',
-    "#",
-    "$",
-    "%",
-    "&",
-    "'",
-    "(",
-    ")",
-    "*",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    ":",
-    ";",
-    "<",
-    "=",
-    ">",
-    "?",
-    "@",
-    "[",
-    "\\",
-    "]",
-    "^",
-    "_",
-    "`",
-    "{",
-    "|",
-    "}",
-    "~",
-  ];
+  let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  let specialCharacters = ["!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
   let collector = [];
 
   //(if statements)
@@ -148,12 +69,6 @@ function random(arr) {
   return i;
 }
 
-//}
-//console.log(collector);
-
-//let string = collector.join("");
-
-//console.log(string);
 
 // Write password to the #password input
 function writePassword() {
@@ -164,8 +79,3 @@ passwordText.value = password;
 
 }
 
-//function writePassword() {
-//let string = collector.join("");
-//let string = document.querySelector("#password");
-
-//}
